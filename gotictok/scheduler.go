cd63e838752e3caeff88c235affdd71003ac8ef8
Evert Joubert <evert.joubert@gmail.com>
2019-08-05 12:29:59 +0200

    Implement lock when executing actions of the firing Job

--- gotictok/scheduler.go
+++ gotictok/scheduler.go
@@ -2,25 +2,36 @@ package gotictok
 
 import (
 	"fmt"
+	"sync"
 	"time"
 )
 
+//Job - Job structure
 type Job struct {
 	lbl       string
 	done      chan bool
 	callfuncs []interface{}
 	intrvl    time.Duration
+	jblck     *sync.Mutex
 }
 
+//ScheduleJob - invoke new Schedulable(Job)
+//label - job label
+//interval - interval in nano seconds
+//callfunc - (optional) ...interface{} list of functions that will be actioned
 func ScheduleJob(label string, interval time.Duration, callfunc ...interface{}) (jb *Job) {
-	jb = &Job{lbl: label, done: make(chan bool), callfuncs: callfunc[:], intrvl: interval}
+	jb = &Job{jblck: &sync.Mutex{}, lbl: label, done: make(chan bool), callfuncs: callfunc[:], intrvl: interval}
 	return
 }
 
+//Run - run or activate Job
 func (jb *Job) Run() *Job {
 	var tck = time.NewTicker(jb.intrvl)
 	var jobaction = func(t time.Time) {
+		jb.jblck.Lock()
+		defer jb.jblck.Unlock()
 		jb.performActions(t)
+
 	}
 	go scheduler(false, tck, jb.done, jobaction)
 	return jb
@@ -56,10 +67,15 @@ func scheduler(doNow bool, tick *time.Ticker, done chan bool, jobaction func(t t
 	}
 }
 
+//Done - job done and can not be used after this
 func (jb *Job) Done() {
 	jb.done <- true
 	<-jb.done
 	jb.callfuncs = nil
 	close(jb.done)
 	jb.done = nil
+	if jb.jblck != nil {
+		jb.jblck = nil
+	}
+	jb = nil
 }
