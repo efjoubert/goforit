47a276008ee52919f00824dd59f293380d790e2f
Evert Joubert <evert.joubert@gmail.com>
2019-08-08 21:10:29 +0200

    Embedded Bootstrap and jquery.Datatables

--- godataagent/godataagentwidget.go
+++ godataagent/godataagentwidget.go
@@ -4,6 +4,8 @@ import (
 	"io"
 	"strings"
 
+	"github.com/efjoubert/goforit/goblet/embed"
+
 	"github.com/efjoubert/goforit/goblet"
 )
 
@@ -18,16 +20,28 @@ func (dtaAgntwdgt *DataAgentWidget) RegisterAgentSession() {
 }
 
 func initDataAgentWidget() {
+	goblet.RegisterEmbededReaders(
+		"/jquery.js", embed.JQueryJS,
+		"/blockui.js", embed.BlockuiJS,
+		"/webactions.js", func() io.Reader { return embed.WebactionsJS(true) },
+		"/bootstrap/css/bootstrap.css", embed.BootstrapCSS,
+		"/bootstrap/js/bootstrap.js")
 	goblet.RegisterServletContextPath("/", "./data").RegisterServlet("/", nil)
 	goblet.RegisterWidgetPath("/data", "agent.html", newDataAgentWidget)
 }
 
 const dataagentwidgethtml string = `<!doctype html>
 <html>
-<head></head>
-<body><span>DATA AGENT</span><body></html>`
+<head>
+<script type="text/javascript" src="/jquery.js"></script>
+<script type="text/javascript" src="/blockui.js"></script>
+<script type="text/javascript" src="/webactions.js"></script>
+</head>
+<body><span>DATA AGENT</span>
+<body></html>`
 
 func dataAgentWidgetHTML() io.Reader {
+
 	return strings.NewReader(dataagentwidgethtml)
 }
 
