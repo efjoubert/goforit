4f284ce4ce87a8a330caa5add4aa186d95cb3e8c
Evert Joubert <evert.joubert@gmail.com>
2019-08-09 20:49:02 +0200

    embed fontawesome all.js

--- godataagent/godataagentwidget.go
+++ godataagent/godataagentwidget.go
@@ -21,6 +21,7 @@ func (dtaAgntwdgt *DataAgentWidget) RegisterAgentSession() {
 
 func initDataAgentWidget() {
 	goblet.RegisterEmbededReaders(
+		"/fontawesome/fontawesome.js", embed.FontAwesomeJS,
 		"/jquery.js", embed.JQueryJS,
 		"/blockui.js", embed.BlockuiJS,
 		"/webactions.js", func() io.Reader { return embed.WebactionsJS(true) },
@@ -45,17 +46,18 @@ const dataagentwidgethtml string = `<!doctype html>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
 <link rel="stylesheet" href="/datatables/css/datatables.css">
+<link rel="stylesheet" href="/hcoffcanvas/css/hcoffcanvas.css">
 <script type="text/javascript" src="/jquery.js"></script>
 <script type="text/javascript" src="/blockui.js"></script>
 <script type="text/javascript" src="/webactions.js"></script>
 <script type="text/javascript" src="/boootstrap/js/bootstrap.js"></script>
 <script type="text/javascript" src="/datatables/js/datatables.js"></script>
+<script type="text/javascript" src="/hcoffcanvas/js/hcoffcanvas.js"></script>
 </head>
 <body><span>DATA AGENT</span>
 <body></html>`
 
 func dataAgentWidgetHTML() io.Reader {
-
 	return strings.NewReader(dataagentwidgethtml)
 }
 
