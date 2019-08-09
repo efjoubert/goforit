74ff86c78bb7b653d1d0dad2f5a77237c53b7d42
Evert Joubert <evert.joubert@gmail.com>
2019-08-09 21:21:52 +0200

    Embed Moment js incl Moment with locals

--- godataagent/godataagentwidget.go
+++ godataagent/godataagentwidget.go
@@ -22,6 +22,8 @@ func (dtaAgntwdgt *DataAgentWidget) RegisterAgentSession() {
 func initDataAgentWidget() {
 	goblet.RegisterEmbededReaders(
 		"/fontawesome/fontawesome.js", embed.FontAwesomeJS,
+		"/moment/moment.js", embed.MomentJS,
+		"/moment/moment-with-locals.js", embed.MomentLocalJS,
 		"/jquery.js", embed.JQueryJS,
 		"/blockui.js", embed.BlockuiJS,
 		"/webactions.js", func() io.Reader { return embed.WebactionsJS(true) },
@@ -47,12 +49,14 @@ const dataagentwidgethtml string = `<!doctype html>
 <head>
 <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
+<link rel="stylesheet" href="/bootstrapdatetime/css/bootstrapdatetime.css">
 <link rel="stylesheet" href="/datatables/css/datatables.css">
 <link rel="stylesheet" href="/hcoffcanvas/css/hcoffcanvas.css">
 <script type="text/javascript" src="/jquery.js"></script>
 <script type="text/javascript" src="/blockui.js"></script>
 <script type="text/javascript" src="/webactions.js"></script>
 <script type="text/javascript" src="/boootstrap/js/bootstrap.js"></script>
+<script type="text/javascript" src="/boootstrapdatetime/js/bootstrapdatetime.js"></script>
 <script type="text/javascript" src="/datatables/js/datatables.js"></script>
 <script type="text/javascript" src="/hcoffcanvas/js/hcoffcanvas.js"></script>
 </head>
