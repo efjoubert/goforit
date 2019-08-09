fb774606d5f6ce4e93dd350ac3df49d41f13972c
Evert Joubert <evert.joubert@gmail.com>
2019-08-09 08:27:31 +0200

    Embed Knockout js

--- godataagent/godataagentwidget.go
+++ godataagent/godataagentwidget.go
@@ -41,6 +41,7 @@ func initDataAgentWidget() {
 const dataagentwidgethtml string = `<!doctype html>
 <html>
 <head>
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
 <link rel="stylesheet" href="/bootstrap/css/bootstrap.css">
 <link rel="stylesheet" href="/datatables/css/datatables.css">
 <script type="text/javascript" src="/jquery.js"></script>
